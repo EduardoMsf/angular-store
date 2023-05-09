542d0e8c64d35a8017d90cde6ef1e56e15ca6958
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-05-09 15:35:56 -0600

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-store';
+  name = 'Eduardo';
+  age = 26;
+  img = 'https://source.unsplash.com/random';
 }
