29bb9d57ca95a03a41f9771e4a6144f2e8e8f16b
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-05-09 14:53:43 -0600

    First commit

---
+++ src/app/recap.ts
@@ -0,0 +1,16 @@
+const username: string = "eduardomsf";
+const sum = ( a: number, b: number) =>{
+  return a + b;
+}
+
+sum(1,3);
+
+class Person {
+
+  constructor( public lastName: string, public age: number){
+
+  }
+
+}
+
+const edu = new Person("Eduardo", 26)
