3bd2c2024b9abec5dcd05e627368d1ca4a6c0596
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-05-09 23:42:59 -0600

    testing ngModel and forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Eduardo';
-  age = 26;
-  img = 'https://source.unsplash.com/random';
+  age = 56;
+  img = 'https://res.cloudinary.com/dheyjds01/image/upload/v1681861536/journal-mui/tzreo0wug9ggvjd7oykf.png';
+  btnDisabled = true;
+  person = {
+    name: "Samaniego",
+    age: 26,
+    avatar: this.img
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
 }
