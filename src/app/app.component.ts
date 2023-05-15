aa69b3e728a5266637d4baf0b34ceb98dc4aaace
Eduardo Samaniego <samaniegomsf@gmail.com>
2023-05-14 19:37:40 -0600

    Product component and stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,28 @@ export class AppComponent {
   onLoaded(img: string) {
     console.log('log padre', img)
   }
+  products: Product[] = [{
+    id: "1",
+    name: "Product 1",
+    img: "https://picsum.photos/200",
+    price: 101
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    img: "https://picsum.photos/200",
+    price: 102
+  },{
+    id: "3",
+    name: "Product 3",
+    img: "https://picsum.photos/200",
+    price: 103
+  },
+  {
+    id: "4",
+    name: "Product 4",
+    img: "https://picsum.photos/200",
+    price: 104
+  }]
 
 }
